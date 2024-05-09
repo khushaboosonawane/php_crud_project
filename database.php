2fa9721a0c768667b18976d7a8814f11f94eb8be
khushaboosonawane <sokhushaboo202@gmail.com>
2024-05-09 13:55:03 +0530

    Data added successfully

--- database.php
+++ database.php
@@ -4,13 +4,14 @@ class Database{
     private $db_username="root";
     private $db_password="";
     private $db_database="school";
-    private $conn=true;
+    private $conn=false;
     private $result=[];
-    private $mysqli="";
+    private $mysqli;
 
     public function __construct(){
        if(!$this->conn){
         $this->mysqli=new mysqli($this->db_host,$this->db_username,$this->db_password,$this->db_database);
+        $this->conn=true;
         if($this->mysqli->connect_error){
             array_push($this->result,$this->mysqli->connect_error);
             return false;
@@ -18,6 +19,43 @@ class Database{
             return true;
         }
        }
+    }
+    public function insert($tableName,$param=array()){
+        if($this->tableExist($tableName)){
+            echo "<pre>";
+            print_r($param);
+            $tablekeys=implode(",",array_keys($param));
+            $tablevalues=implode("' , '", $param);
+
+            $sql="INSERT INTO $tableName ($tablekeys) VALUES ('$tablevalues')";
+            $data=$this->mysqli->query($sql);
+            if($data){
+              array_push($this->result,$this->mysqli->insert_id);
+              return true;
+            }else{
+                array_push($this->result,$this->mysqli->error);
+                return false;
+            }
+
+        }
+
+    }
+    public function getResult(){
+        $val=$this->result;
+        $this->result=array();
+        return $val;
+    }
+    private function tableExist($table_name){
+        $sql="SHOW TABLES FROM $this->db_database LIKE '$table_name'";
+        $tableInDb=$this->mysqli->query($sql);
+        if($tableInDb){
+            if($tableInDb->num_rows==1){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
     }
 
     public function __destruct(){
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include("database.php");
 $obj=new Database();
-
+$obj->insert("student",['student_name'=>"khushaboo",'student_mobile'=>'9665065113','student_email'=>"sokhushaboo202@gmail.com","student_pss"=>"123456"]);
+print_r($obj->getResult());
 ?>
