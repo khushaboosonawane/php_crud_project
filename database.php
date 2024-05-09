2e97ba7a1b009111c9960a0988d466942ff50760
khushaboosonawane <sokhushaboo202@gmail.com>
2024-05-09 12:35:55 +0530

    connection code write successfully

---
+++ database.php
@@ -0,0 +1,35 @@
+<?php
+class Database{
+    private $db_host="localhost";
+    private $db_username="root";
+    private $db_password="";
+    private $db_database="school";
+    private $conn=false;
+    private $result=[];
+    private $mysqli="";
+
+    public function __construct(){
+       if(!$this->conn){
+        $this->mysqli=new connect($this->db_host,$this->db_username,$this->db_password,$this->db_database);
+        if($this->mysqli->connect_error){
+            array_push($this->result,$this->mysqli->connect_error);
+            return false;
+        }else{
+            return true;
+        }
+       }
+    }
+
+    public function __destruct(){
+        if($this->conn){
+          if($this->mysqli->close()){
+            $this->conn=false;
+            return true;
+          }
+        }
+        else{
+            return false;
+        }
+    }
+}
+?>
